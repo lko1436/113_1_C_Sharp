2ef2077976590312e488888117c11061bde94969
廖呈紘 <152866042+lko1436@users.noreply.github.com>
2024-11-26 18:01:17 +0800

    11-26

---
+++ 113-11-26/Tutorial 5-1/Ending Balance/Ending Balance/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ending_Balance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            const decimal INTEREST_RATE = 0.005m; // 用來儲存利率。
+            decimal balance; // 用來儲存起始餘額。
+            int months; // 用來儲存月數。
+            
+            if (decimal.TryParse(startingBalTextBox.Text, out balance))
+            { 
+                // 使用迴圈計算每個月的餘額。
+                if (int.TryParse(monthsTextBox.Text, out months))
+                {
+                    int count = 1; // 用來計算月份。
+                    while (count <= months)
+                    {
+                        // 計算每個月的餘額。
+                        balance = balance + (balance * INTEREST_RATE);
+                        count += 1;
+                    }
+
+                    // 顯示最後的餘額
+                    endingBalanceLabel.Text = balance.ToString("c");
+
+                }
+                else
+                {
+                    // 顯示錯誤訊息。
+                    MessageBox.Show("輸入[月分數量]資料格式錯誤");
+                }
+            }
+            else
+            {
+                
+                // 顯示錯誤訊息。
+                MessageBox.Show("輸入[起始餘額]資料格式錯誤");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // 清除 TextBoxes 和 endingBalanceLabel 控制項的內容。
+            startingBalTextBox.Text = "";
+            monthsTextBox.Text = "";
+            endingBalanceLabel.Text = "";
+
+            // 重設焦點。
+            startingBalTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單。
+            this.Close();
+        }
+    }
+}
---
+++ 113-11-26/Tutorial 5-4/Friend File/Friend File/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Friend_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void writeNameButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 建立一個 StreamWriter 物件，用來寫入檔案。
+                StreamWriter outputFile;
+
+                // 開啟 Friends.txt 檔案，如果檔案不存在，則建立檔案。
+                outputFile = File.AppendText(@"C:\Users\shu\Desktop\Friends.txt");
+
+                // 將名字寫入檔案。
+                outputFile.WriteLine(nameTextBox.Text);
+
+                // 關閉檔案。
+                outputFile.Close();
+
+                // 清除文字方塊。
+                nameTextBox.Text = "";
+
+                // 顯示訊息。
+                MessageBox.Show("名字已經寫入檔案。");
+            }
+           
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單。
+            this.Close();
+        }
+    }
+}
