db1d59d67352f3f14d21ab55060025cae35cd2d1
廖呈紘 <152866042+lko1436@users.noreply.github.com>
2024-12-03 18:01:56 +0800

    12-03

---
+++ 113-12-03/Tutorial 5-5-1/Friend File/Friend File/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Friend_File
+{
+    public partial class Form1 : Form
+    {
+        private SaveFileDialog saveFile; // 新增 SaveFileDialog 變數
+
+        public Form1()
+        {
+            InitializeComponent();
+            saveFile = new SaveFileDialog(); // 初始化 SaveFileDialog
+        }
+
+        private void writeNameButton_Click(object sender, EventArgs e)
+        {
+            // 這裡是寫入名字的按鈕點擊事件處理程式
+            try
+            {
+                StreamWriter outputFile;
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    outputFile = File.AppendText(saveFile.FileName);
+                    outputFile.WriteLine(nameTextBox.Text);
+                    outputFile.Close();
+                    MessageBox.Show("名字已經寫入檔案");
+                }
+                else
+                {
+                    MessageBox.Show("使用者取消了操作");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
---
+++ 113-12-03/Tutorial 5-6/South America/South America/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace South_America
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getCountriesButton_Click(object sender, EventArgs e)
+        {
+            // 這裡是取得國家按鈕的點擊事件處理程式。
+            try
+            {
+                // 建立 StreamReader 物件，用來讀取國家清單。
+                StreamReader inputFile;
+                inputFile = File.OpenText("Countries.txt");
+                countriesListBox.Items.Clear();
+
+                // 讀取檔案，直到檔案結尾。
+                while (!inputFile.EndOfStream)
+                {
+                    // 讀取檔案中的一行。
+                    countriesListBox.Items.Add(inputFile.ReadLine());
+                }
+
+                // 關閉檔案。
+                inputFile.Close();
+            }
+            catch (Exception ex)
+            {
+                // 顯示錯誤訊息。
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單。
+            this.Close();
+        }
+    }
+}
